bdba288939a2e79585d14fd1a2204226c98bb490
dreadlord-sedai <dahamifabbio@gmail.com>
2025-06-06 20:17:25 +0530

    Declare variables

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -8,9 +8,20 @@ public class Calculator implements ActionListener {
     JFrame frame;
     JTextField textField;
     JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton;
+    JPanel panel;
+
+    Font myFont = new Font("Serif", Font.BOLD, 30);
+    double num1 = 0, num2 = 0, result = 0;
+    char operator;
 
     Calculator() {
 
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
     }
 
     public static void main(String[] args) {
